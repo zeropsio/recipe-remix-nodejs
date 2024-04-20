77526897f0c7d346a11f4714c8447120195d36e2
Ales Rechtorik <foxxcz@gmail.com>
2024-04-20 14:53:57 +0200

    initial commit

---
+++ app/components/Register.tsx
@@ -0,0 +1,11 @@
+export function Register() {
+  return (
+    <div className="fixed hidden md:block bottom-20 right-5 z-50 items-center justify-center">
+      <a href="https://app.zerops.io/registeration" target="_blank">
+        <div className=" font-bold rounded-md bg-[#3b82f6] p-3 px-5 py-3 font-mono text-xs hover:bg-[#4579f1] text-[#fff] duration-100 ">
+          Register and Get up to $100 free Credit 🎉{" "}
+        </div>
+      </a>
+    </div>
+  );
+}
---
+++ app/root.tsx
@@ -0,0 +1,37 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+import styles from "./styles/main.css?url";
+import { Register } from "./components/Register";
+
+export const links = () => {
+  return [{ rel: "stylesheet", href: styles }];
+};
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body className="font-opensans">
+        {children}
+        <Register />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  return <Outlet />;
+}
