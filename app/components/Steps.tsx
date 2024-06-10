718c2f4740abae1e5df4d139d8d8d9430f59482e
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-06-11 01:38:00 +0530

    feat: add deploy badge img

--- app/components/Steps.tsx
+++ app/components/Steps.tsx
@@ -41,11 +41,11 @@ services:
         Deploy to Zerops with a Single Click
       </h2>
         <a className="badge flex justify-center hover:-translate-y-1 hover:duration-300 pb-5" href="https://app.zerops.io/recipe/remix" target="_blank">
-          <img width="300px" draggable="false" src="https://private-user-images.githubusercontent.com/1303561/335229072-21cf77dd-cded-4e41-8e76-24540a809ccc.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTgwMzk1MDAsIm5iZiI6MTcxODAzOTIwMCwicGF0aCI6Ii8xMzAzNTYxLzMzNTIyOTA3Mi0yMWNmNzdkZC1jZGVkLTRlNDEtOGU3Ni0yNDU0MGE4MDljY2MucG5nP1gtQW16LUFsZ29yaXRobT1BV1M0LUhNQUMtU0hBMjU2JlgtQW16LUNyZWRlbnRpYWw9QUtJQVZDT0RZTFNBNTNQUUs0WkElMkYyMDI0MDYxMCUyRnVzLWVhc3QtMSUyRnMzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNDA2MTBUMTcwNjQwWiZYLUFtei1FeHBpcmVzPTMwMCZYLUFtei1TaWduYXR1cmU9Y2UyMzQ1NmNhNWM1ZTQxYTc5ODhkNDRlMzAwZTJiYWIyZDdkOTM1NDZhNDVjZmRmNTRmYWY3OGExNzVlNTRmNyZYLUFtei1TaWduZWRIZWFkZXJzPWhvc3QmYWN0b3JfaWQ9MCZrZXlfaWQ9MCZyZXBvX2lkPTAifQ.iwgbhkF2hoJaZQ2IbU8OQvCn4VRkML-0GeqraEbRY18" alt="deploy on zerops with one click badge" />
+          <img width="300px" draggable="false" src="deploy.png"  alt="deploy on zerops with one click badge" />
         </a>
       <div className="flex justify-center pb-5">
         <svg height="100px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M12 4V20M12 20L8 16M12 20L16 16" stroke="#F0F0F0" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" />
+          <path d="M12 4V20M12 20L8 16M12 20L16 16" stroke="#F0F0F0" stroke-width="1.3" stroke-linecap="round" strokeLinejoin="round" />
         </svg>
         </div>
         <p className="max-w-lg text-center mx-auto">Deploying will import the following structure and use following instructions to build and deploy </p>
