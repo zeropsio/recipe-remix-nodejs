67f2369f61b38426bcd6ff3710758c6d5cfcf873
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-06-10 22:02:52 +0530

    feat: remix v2 ready

--- app/components/Register.tsx
+++ app/components/Register.tsx
@@ -3,7 +3,7 @@ export function Register() {
     <div className="fixed hidden md:block bottom-20 right-5 z-50 items-center justify-center">
       <a href="https://app.zerops.io/registeration" target="_blank">
         <div className=" font-bold rounded-md bg-[#3b82f6] p-3 px-5 py-3 font-mono text-xs hover:bg-[#4579f1] text-[#fff] duration-100 ">
-          Register and Get up to $100 free Credit 🎉{" "}
+          Register and Get up to $60 free Credit 🎉{" "}
         </div>
       </a>
     </div>
